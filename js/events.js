cbf93c9e865eaa3a42e9c7c2ef1cf534b0368cff
“Paul <prowori2@gmail.com>
2019-03-22 13:31:46 +0300

    Finished the events tutorial part 1

--- js/events.js
+++ js/events.js
@@ -1,6 +1,14 @@
-// Function to modify the text content of the paragraph
-const changeText = () => {
-    const p = document.querySelector('p');
+const p = document.querySelector('p');
+const button = document.querySelector('button');
 
-    p.textContent = "I changed because of an inline event handler.";
+const changeText = () => {
+    p.textContent = "Will I change?";
 }
+
+const alertText = () => {
+    alert('Will I alert?');
+}
+
+// Multiple listeners can be added to the same event and element
+button.addEventListener('click', changeText);
+button.addEventListener('click', alertText);
